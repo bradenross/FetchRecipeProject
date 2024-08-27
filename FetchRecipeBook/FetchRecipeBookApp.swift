5d235413f12c8d35c5306bb043880dac29be7649
bradenross <bradenro55@yahoo.com>
2024-08-27 12:07:47 -0500

    Initial Commit

---
+++ FetchRecipeBook/FetchRecipeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeBookApp.swift
+//  FetchRecipeBook
+//
+//  Created by Braden Ross on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
